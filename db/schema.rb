2fd92be058ad54818ebf35d5b6f6c239a9195898
alexgriff <alexalexgriffith@gmail.com>
2018-02-08 15:39:24 -0500

    first half

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180208193610) do
+ActiveRecord::Schema.define(version: 20180208201941) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180208193610) do
     t.datetime "publication_date"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "author_id"
   end
 
 end
--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,7 @@
 class Book < ActiveRecord::Base
+  belongs_to :author
 
-  def author
-    Author.find_by(id: self.author_id)
-  end
+  # def author
+  #   Author.find_by(id: self.author_id)
+  # end
 end
